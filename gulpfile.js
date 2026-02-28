3cff35d6aa3bdf1a4bea9dec717bf508ed0ddb02
k2works <kakimomokuri@gmail.com>
2026-02-28 10:34:56 +0900

    chore: ビルドツール・開発環境設定を追加
    Gulp タスク、npm lock ファイル、Nix flake、運用スクリプト、アプリケーション構成を追加

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Gulpfile that loads tasks from the script directory
+ */
+
+import 'dotenv/config';
+import gulp from 'gulp';
+import mkdocsTasks from './ops/scripts/mkdocs.js';
+import journalTasks from './ops/scripts/journal.js';
+import vaultTasks from './ops/scripts/vault.js';
+import sshTasks from './ops/scripts/ssh.js';
+
+// Load gulp tasks from script modules
+mkdocsTasks(gulp);
+journalTasks(gulp);
+vaultTasks(gulp);
+sshTasks(gulp);
+
+export const dev = gulp.series('mkdocs:serve', 'mkdocs:open');
+
+// Export gulp to make it available to the gulp CLI
+export default gulp;
